879f012384ba00acb2f4277bd65b352158b5ecc8
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2024-09-04 08:32:12 -0300

    Update Main.java

--- java/src/pkg/Main.java
+++ java/src/pkg/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int quantidade = 60000; 
 
         Random random = new Random();
-        int tamanhoNome = random.nextInt(3, 6); 
+        int tamanhoNome = random.nextInt(3, 4); 
 
         listaBolha = Util.gerarNomesEIdades(quantidade, tamanhoNome);
         listaSelecao = Util.gerarNomesEIdades(quantidade, tamanhoNome);
